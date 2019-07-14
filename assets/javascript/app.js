1f7d3ab016ae27db8df9c02c763c61309d1ee9a7
RocioE <48306304+RocioE@users.noreply.github.com>
2019-07-13 17:13:06 -0700

    created buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,60 +7,62 @@
     var score= 0;//how many questions the user got right//
     var answerCorrect;
     var answerWrong;
+ 
 
-    for(var i= 0; i <questions.length; 1++){
-
-    }
-
-    let questions= [//this is an array of question objects//
+    var questions= [//this is an array of question objects//
         {
             question : "In which town do the Simpsons reside?",
-            choiceA : "Springfield",
-            choiceB : "Shelbyville",
-            choiceC : "Seinfeld",
+            choices : ["Springfield", "Shelbyville", "Seinfeld"],
             answer: "Springfield",
         },
         {
             question : "Who founded the Simpsons' town?",
-            choiceA : "Jebadiah Springfield",
-            choiceB : "Zachariah Springfield",
-            choiceC : "Springfield Manhattan",
+            choices : ["Jebadiah Springfield", "Zachariah Springfield", "Springfield Manhattan"],
             answer: "Jebadiah Springfield",
         },
         {
             question : "What is the name of Lisa's jazz mentor?",
-            choiceA : "Billy Jazzman",
-            choiceB: "Blind Willy Witherspoon",
-            choiceC: "Bleeding Gums Murphy",
+            choices : ["Billy Jazzman", "Blind Willy Witherspoon", "Bleeding Gums Murphy"],
             answer: "Bleeding Gums Murphy",
             
         },
         {
             question : "What is the name of the minister at the First Church of Springfield?",
-            choiceA : "Ned Flanders",
-            choiceB : "Timothy Lovejoy",
-            choiceC : "Carl Carlson",
+            choices : ["Ned Flanders", "Timothy Lovejoy", "Carl Carlson"],
             answer: "Timothy Lovejoy",
 
         },
         {
             question : "What is the name of Mr. Burns' teddy bear?",
-            choiceA: "Nono",
-            choiceB: "Gogo",
-            choiceC: "Bobo",
+            choices: ["Nono", "Gogo", "bobo"],
             answer: "Bobo",
         },
 
     ];
-            function checkAnswer(answer){ //here is where it check for correct anser//
-                if(anser ==question[runningQuestion].correct){  //this checks within array//
-                    //answer is correct// 
-                    if (runningQuestion < lastQuestion){
-                        runningQuestion++;
-                        renderQuestion();
-                    }
-
+    function checkAnswer(Useranswer,question){ //here is where it check for correct anser//
+        if(Useranswer == question){  //this checks within array//
+            console.log("correct")
             }
+
+    }
+   
+
+    for(var i= 0; i<questions.length; i++){
+      var button=$("<button>");
+      var question= questions [i]
+     // button.attr("data-name",questions[i].choices[i]);
+     var header= $("<h1>").text(question.question)
+      console.log(header)
+        $("#query").append(header)
+        
+    }
+    
+ 
+           $("button").on("click", function(){
+               $(this).attr("data-name")
+
+
+           })
 
             function answerCorrect(){
                 document.getElementById(runningQuestion);
@@ -69,10 +71,10 @@
 
             function answerWrong(){
                 document.getElementById(runningQuestion)
-
             }
-            StaticRange.addEventListener("click.")//add event listener
 
+
+         
             function startQuiz(){  //need to create function to start game//
                 startQuiz.style.display="none";
                 renderQuestion();
@@ -84,7 +86,7 @@
 
 
          //!this keeps track of the running question//
-         const lastQuestion = questions, length, -1;//! ahould it be at zero???
+         //const lastQuestion = questions, length, -1;//! ahould it be at zero???
          let runningQuestion = "0";   
 
 
@@ -98,4 +100,3 @@
              choiceC.innerHTML = q.choiceC; //The innerHTML property is part of the Document Object Model (DOM),allows Js code to manipulate a website being displayed. Specifically, it allows reading and replacing everything within a given DOM element
 console.log(choice)
          }
-   
